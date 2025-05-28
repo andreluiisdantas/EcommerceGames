90a0539d894267fe467e8766ef5ff8b6cc078188
Andre Luis <133072663+andreluiisdantas@users.noreply.github.com>
2025-05-28 08:47:10 -0300

    Delete Sistema directory

--- Sistema/banco/conexao.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import os
-
-def conectar():
-    caminho_banco = os.path.join(os.path.dirname(__file__), 'database.db')
-    conexao = sqlite3.connect(caminho_banco)
-    return conexao
